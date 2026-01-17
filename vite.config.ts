d0a1686399da9c4fbaa9ed5fbc08d4731c357b6e
GabrielGB-web <punisherjogador@gmail.com>
2026-01-16 21:38:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
